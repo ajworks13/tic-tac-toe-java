7e57e888f673b8dbcb5b9ef4975582b2cb044d7f
AJ <63066039+ajworks13@users.noreply.github.com>
2022-06-15 18:59:22 -0400

    Update java-tac-toe.java
    Trying to figure out how to handle sending the choices into another method. Mainly the gameboard method.

--- java-tac-toe.java
+++ java-tac-toe.java
@@ -4,8 +4,10 @@ public class Main
 {
     //Global vars
     static Scanner sc = new Scanner(System.in);
+    static boolean gameOver = false;
     
-    static void gameBoard(){
+    static void gameBoard(String p1, String p2, String decision){
+        
         String[][] theGame = {
             {"_","_","_"},
             {"_","_","_"},
@@ -22,9 +24,21 @@ public class Main
     
     public static void main(String[] args) {
         System.out.println(" -- TICK TAC TOE --");
-        System.out.println("Player 1, GO! (You are X)\n Press a single number between 1-9.");
-        gameBoard();
-        int p1Choice = sc.nextInt();
+        
+        do{
+            System.out.println("Player 1, GO! (You are X)\n Press a single number between 1-9.");
+            gameBoard();
+            int p1Choice = sc.nextInt();
+            
+            if(p1Choice < 1 || p1Choice > 9){
+                System.out.println("Can NOT exceed a number grader than 9 or less than 1.\nTry again..");
+            }
+            if(p1Choice == 1){
+                gameBoard(p1Choice,p2Choice,theDecision);
+            }
+            
+            
+        }while(gameOver != true);
         
         
     }
