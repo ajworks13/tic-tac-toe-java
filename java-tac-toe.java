df8dae963d087126673bcd9e43344ed4cd984fe8
AJ <63066039+ajworks13@users.noreply.github.com>
2022-06-16 18:05:34 -0400

    Update java-tac-toe.java
    Changing things around with decision making.

--- java-tac-toe.java
+++ java-tac-toe.java
@@ -1,45 +1,75 @@
 import java.util.*;
 
-public class Main
-{
-    //Global vars
-    static Scanner sc = new Scanner(System.in);
-    static boolean gameOver = false;
-    
-    static void gameBoard(String p1, String p2, String decision){
-        
-        String[][] theGame = {
-            {"_","_","_"},
-            {"_","_","_"},
-            {"_","_","_"}
-        };
-        
-        for (int i = 0; i < theGame.length; ++i) {
-          for(int j = 0; j < theGame[i].length; ++j) {
-              System.out.print(theGame[i][j]);
-          }
-          System.out.println("");
-        }
-    }
-    
-    public static void main(String[] args) {
-        System.out.println(" -- TICK TAC TOE --");
-        
-        do{
-            System.out.println("Player 1, GO! (You are X)\n Press a single number between 1-9.");
-            gameBoard();
-            int p1Choice = sc.nextInt();
-            
-            if(p1Choice < 1 || p1Choice > 9){
-                System.out.println("Can NOT exceed a number grader than 9 or less than 1.\nTry again..");
-            }
-            if(p1Choice == 1){
-                gameBoard(p1Choice,p2Choice,theDecision);
-            }
+public class Main {
+	//Global vars
+	static Scanner sc = new Scanner(System.in);
+	static boolean gameOver = false;
+
+
+	public static void main(String[] args) {
+		System.out.println(" -- TICK TAC TOE --");
+
+		String[][] theGame = {
+			{"_", "_", "_"},
+			{"_", "_", "_"},
+			{"_", "_", "_"}
+		};
+
+		do {
+			System.out.println("Player 1, GO! (You are X)\n Press a single number between 1-9.");
+			int p1Choice = sc.nextInt();
+
+			if (p1Choice < 1 || p1Choice > 9) {
+				System.out.println("\nCan NOT exceed a number grader than 9 or less than 1.\nTry again..");
+			}
             
             
-        }while(gameOver != true);
+			if (p1Choice == 1) {
+				theGame[0][0] = "X";
+			}else if(p1Choice == 2){
+			     theGame[0][1] = "X";   
+			}
+			
+			
+            
+            //Prints the board game.
+			for (int i = 0; i < theGame.length; ++i) {
+				for (int j = 0; j < theGame[i].length; ++j) {
+					System.out.print(theGame[i][j]);
+				}
+				System.out.println("");
+			}
+			
+			System.out.println("Player 2, GO! (You are O)\nPress a single number between 1-9.");
+			int p2Choice = sc.nextInt();
+			
+			if (p2Choice < 1 || p2Choice > 9) {
+				System.out.println("\nCan NOT exceed a number grader than 9 or less than 1.\nTry again..");
+			}
+			
+			//-------------------Check if p1 already selected it--------------
+			if(theGame[0][0] == "X" && p2Choice == 1){
+			        
+			     while(p2Choice == 1){
+			             System.out.println("\nPlayer 1 already selected that. Try another number..\n");
+			             p2Choice = sc.nextInt();
+			             
+			             if(p2Choice != 1){
+			                  break;   
+			             }
+			     }
+			}
+			//--------------------------------------------------------------------------------------
+			
+			if (p2Choice == 1) {
+				theGame[0][0] = "O";
+			}else if(p2Choice == 2){
+			     theGame[0][1] = "O";   
+			}
+			
         
-        
-    }
+		} while (gameOver != true);
+
+
+	}
 }
