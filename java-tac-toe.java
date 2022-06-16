891c3b38e47f14b7705ee3f80380650357db0012
AJ <63066039+ajworks13@users.noreply.github.com>
2022-06-16 19:47:50 -0400

    Update java-tac-toe.java
    Added some more troubleshooting. Still working on picking up on selected numbers.

--- java-tac-toe.java
+++ java-tac-toe.java
@@ -9,6 +9,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		ArrayList<Integer> numbersUsed = new ArrayList<Integer>();
+		int p1Choice = -1;
+		int p2Choice = -1;
 		
 		System.out.println(" -- TICK TAC TOE --");
 
@@ -20,7 +22,7 @@ public class Main {
 
 		do {
 			System.out.println("Player 1, GO! (You are X)\n Press a single number between 1-9.");
-			int p1Choice = sc.nextInt();
+			p1Choice = sc.nextInt();
             
             //----------------------------------- MAKE SURE NO OTHER NUMBER IS BEING ENTERED --------------------------------
 			if (p1Choice < 1 || p1Choice > 9) {
@@ -36,6 +38,21 @@ public class Main {
 			    }while(true);
 			}
             //-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            
+            //-----------------------------------------------------CHECK IF PLAYER 2 SELECTED IT ALREADY-----------
+            
+            if(numbersUsed.contains(1) || numbersUsed.contains(2) || numbersUsed.contains(3) || numbersUsed.contains(4) || numbersUsed.contains(5) || numbersUsed.contains(6) || numbersUsed.contains(7) || numbersUsed.contains(8) || numbersUsed.contains(9)){
+			     while(true){
+			         System.out.println("That number is already being used.\nTry another number..");
+			         p1Choice = sc.nextInt();   
+			         if(p1Choice != p2Choice){
+			              break;   
+			         }
+			     }
+			     
+			}
+			
+			//-----------------------------------------------------------------------------------------------------------------------------------------------
             
 			if (p1Choice == 1) {
 				theGame[0][0] = "X";
@@ -54,6 +71,25 @@ public class Main {
 			          System.out.print(" [" + x + " ]");   
 			     }
 			     System.out.println("\n");
+			     
+			}else if(p1Choice == 3){
+			     theGame[0][2] = "X";   
+			     numbersUsed.add(3);
+			     
+			     for(int x: numbersUsed){
+			          System.out.print(" [" + x + " ]");   
+			     }
+			     System.out.println("\n");
+			     
+			}else if(p1Choice == 4){
+			     theGame[1][0] = "X";   
+			     numbersUsed.add(4);
+			     
+			     for(int x: numbersUsed){
+			          System.out.print(" [" + x + " ]");   
+			     }
+			     System.out.println("\n");
+			     
 			}
 			
 			
@@ -69,7 +105,7 @@ public class Main {
 			
 			//----------- PLAYER 2 GOES NOW ---------------------------------------------------------------------------------------------------------------------------------------------
 			System.out.println("Player 2, GO! (You are O)\nPress a single number between 1-9.");
-			int p2Choice = sc.nextInt();
+			p2Choice = sc.nextInt();
 			
 			
 			//----------------------------------- MAKE SURE NO OTHER NUMBER IS BEING ENTERED --------------------------------
@@ -89,50 +125,54 @@ public class Main {
 			
 			
 			//-------------------Check if p1 already selected it--------------
-			
-			if(numbersUsed.contains(1) || numbersUsed.contains(2) || numbersUsed.contains(3) || numbersUsed.contains(4) || numbersUsed.contains(5) || numbersUsed.contains(6) || numbersUsed.contains(7) || numbersUsed.contains(8) || numbersUsed.contains(9)){
-			     while(true){
-			         System.out.println("That number is already being used.\nTry another number..");
-			         p2Choice = sc.nextInt();   
-			         if(p2Choice != p1Choice){
-			              break;   
-			         }
-			     }
-			     
+			if(p2Choice == p1Choice){
+			        System.out.println("That number is already being used.\nTry another number..");
+			        if(numbersUsed.contains(1) || numbersUsed.contains(2) || numbersUsed.contains(3) || numbersUsed.contains(4) || numbersUsed.contains(5) || numbersUsed.contains(6) || numbersUsed.contains(7) || numbersUsed.contains(8) || numbersUsed.contains(9)){
+        			     while(true){
+        			         System.out.println("That number is already being used.\nTry another number..");
+        			         p2Choice = sc.nextInt();   
+        			         if(p2Choice != p1Choice){
+        			              break;   
+        			         }
+        			     }
+        			     
+        			}
 			}
 			
-			
-			
-			
-			
-			/*
-			if(theGame[0][0] == "X" && p2Choice == 1){
-			        
-			     while(p2Choice == 1){
-			             System.out.println("\nPlayer 1 already selected that. Try another number..\n");
-			             p2Choice = sc.nextInt();
-			             
-			             for (int i = 0; i < theGame.length; ++i) {
-                				for (int j = 0; j < theGame[i].length; ++j) {
-                					System.out.print(theGame[i][j]);
-                				}
-                				System.out.println("");
-                		  }
-                		  
-			             if(p2Choice != 1){
-			                  break;   
-			             }
-			     }// while
-			}
-			*/
 			
 			
 			//--------------------------------------------------------------------------------------
 			
 			if (p2Choice == 1) {
 				theGame[0][0] = "O";
+				numbersUsed.add(1);
+				
+				for(int x: numbersUsed){
+			          System.out.print(" [" + x + " ]");   
+			     }
 			}else if(p2Choice == 2){
-			     theGame[0][1] = "O";   
+			     theGame[0][1] = "O";
+			     numbersUsed.add(2);
+			     
+			     for(int x: numbersUsed){
+			          System.out.print(" [" + x + " ]");   
+			     }
+			}else if(p2Choice == 3){
+			     theGame[0][2] = "O";   
+			     numbersUsed.add(3);
+			     
+			     for(int x: numbersUsed){
+			          System.out.print(" [" + x + " ]");   
+			     }
+			     System.out.println("\n");
+			}else if(p2Choice == 4){
+			     theGame[1][0] = "O";   
+			     numbersUsed.add(4);
+			     
+			     for(int x: numbersUsed){
+			          System.out.print(" [" + x + " ]");   
+			     }
+			     System.out.println("\n");
 			}
 			
 			for (int i = 0; i < theGame.length; ++i) {
